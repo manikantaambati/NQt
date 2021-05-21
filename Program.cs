5dc9a03cb3189e40be2ea9d8463f3e477964b55c
manikantaambati <84079527+manikantaambati@users.noreply.github.com>
2021-05-21 15:45:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NUnitMoqHandson1
+{
+    public class Calculator
+    {
+        public int Add(int firstNumber, int secondNumber)
+        {
+            if (firstNumber > 0 && secondNumber > 0)
+            {
+                return firstNumber + secondNumber;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            Console.WriteLine(c.Add(5, 7));
+        }
+    }
+}
